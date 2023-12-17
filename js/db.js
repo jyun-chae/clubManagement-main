21b24d24d6d157f87b452e316df23ce8f7b5ed88
jyun-chae <145102478+jyun-chae@users.noreply.github.com>
2023-12-17 19:52:08 +0900

    Update db.js

--- js/db.js
+++ js/db.js
@@ -6,26 +6,7 @@ var studentList;
 
 function loadClubList(){
     url = 'https://script.google.com/macros/s/AKfycbz48SfrCNoyzAC2Y4cassb96sI35bHVK6wXZ12gB4x9yr6JPzBeerH7Tj_DONO6js4ziQ/exec'
-    fetch(url)
-        .then(response => response.json())
-        .then(json => {
-            data = json.list
-            var clubExplain = document.getElementById('clubExplain')
-            var firstList = document.getElementById('firstList')
-            var secondList = document.getElementById('secondList')
-            clubExplain.innerHTML = ''
-            for (let i in data) {
-                if (data[i].group == 1) {
-                    firstList.innerHTML += `<div class="${i}" onclick="moveY(${i})">${data[i].name}</div>`
-                } else if (data[i].group == 2) {
-                    secondList.innerHTML += `<div class="${i}" onclick="moveY(${i})">${data[i].name}</div>`
-                }
-                clubExplain.innerHTML += clubContainer(data[i])
-            }
-            console.log(data)
-        }).catch(() => {
-            console.log('error')
-    });
+
     fetch('https://script.google.com/macros/s/AKfycbyoVFjOc0iMdrnCL2leJvQ3lwWMcIUm9CeJyVCC35_WKyDIdpFI7gOWoHoGPchd9ReBeQ/exec')
         .then(response => response.json())
         .then(json => {
